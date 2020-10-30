5cf38cbb8a6af4f3f9e55af00bf3ae4d05f4de5c
ChrisNJ <39505428+ChrisNJ@users.noreply.github.com>
2020-10-30 17:05:34 -0400

    updated db,js

--- db.js
+++ db.js
@@ -1,21 +1,12 @@
-// const Pool = require("pg").Pool;
-// require("dotenv").config();
-// const pool = new Pool({
-//   connectionString: proConfig,
-// });
-
-// const proConfig =
-//   "postgres://tcrcxuzu:NZYbl_mSwjr4d7WTZZyhHHewjosYj0AO@lallah.db.elephantsql.com:5432/tcrcxuzu";
-// module.exports = pool;
-
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://tcrcxuzu:NZYbl_mSwjr4d7WTZZyhHHewjosYj0AO@lallah.db.elephantsql.com:5432/tcrcxuzu");
+const db = new Sequelize(
+  "postgres://tcrcxuzu:NZYbl_mSwjr4d7WTZZyhHHewjosYj0AO@lallah.db.elephantsql.com:5432/tcrcxuzu"
+);
 
 //Test DB
 db.authenticate()
   .then(() => console.log("Database conntected...."))
   .catch((err) => console.log("Error: " + err));
-
 
 module.exports = db;
--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
-function jwtGenerator(id){
-    const payload = {
-        user: id
-    }
+function jwtGenerator(id) {
+  const payload = {
+    user: id,
+  };
 
-    return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "4hr" })
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "4hr" });
 }
 
 module.exports = jwtGenerator;
