ef7b544338b5098d4b5c35fa62b777e7e7c4af65
Seth Timothy <timothy.seth101@gmail.com>
2020-11-14 01:37:35 -0400

    created json files, models and initDB

---
+++ initDB.js
@@ -0,0 +1,60 @@
+// initializes database with json files
+// populates the respective tables with data
+
+const fs = require("fs"); // file system module to read data from files
+const db = require("./db");
+
+// imports models to create table records
+//const Product = require("./models/Product");
+const Style = require("./models/Style");
+//const Type = require("./models/Type");
+
+// reads the products.json file and creates records in the Product table
+// fs.readFile("./data/products.json", "utf-8", (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   const products = JSON.parse(data.toString());
+
+//   for (var i = 0; i < products.length; i++){
+//     Product.create({
+//       name: products[i].name,
+//       brand: products[i].brand,
+//       type: products[i].type,
+//       description: products[i].description,
+//       imageUrl: products[i].imageUrl
+//     });
+//   }
+// });
+
+// reads the types.json file and creates records in the Type table
+// fs.readFile("./data/types.json", "utf-8", (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   const types = JSON.parse(data.toString());
+
+//   for (var i = 0; i < types.length; i++){
+//     Type.create({
+//       type: types[i].type,
+//       category: types[i].category
+//     });
+//   }
+// });
+
+// reads the styles.json file and creates records in the Style table
+fs.readFile("./data/styles.json", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const styles = JSON.parse(data.toString());
+
+  for (var i = 0; i < styles.length; i++){
+    Style.create({
+      name: styles[i].name,
+      type: styles[i].type,
+      hairLength: styles[i].hairLength,
+      hairType: styles[i].hairType
+    });
+  }
+});
