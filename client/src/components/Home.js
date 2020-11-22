bc49dc63dff5251315e57677262aed53a6ed7601
Dexter/Flarezard <54971082+Flarezard@users.noreply.github.com>
2020-11-22 15:29:25 -0400

    Testing
    will this even work?

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import React from 'react';
+import logo from './logo.png'; // Tell webpack this JS file uses this image
+
+console.log(logo); // /logo.84287d09.png
 
 class Home extends Component {
   render() {
