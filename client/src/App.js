3923c1fbfa7184191a8509c65355113e7e477891
Dexter/Flarezard <54971082+Flarezard@users.noreply.github.com>
2020-11-23 13:54:31 -0400

    Line 15 App.js Route path = "./" changed to "/" due to errors

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./components/Main";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <NavBar />
 
           <Switch>
+            <Home></Home>
             <Route path="/" component={Main} />
           </Switch>
         </Router>
