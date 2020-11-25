e6325dededcd87123690c1ada6c482f6c65e24dc
Seth Timothy <timothy.seth101@gmail.com>
2020-11-24 20:44:19 -0400

    added form to profile page

--- client/src/components/FormPage.js
+++ client/src/components/FormPage.js
@@ -1,163 +1,155 @@
-import React, { Component } from "react";
+import { Formik } from "formik";
+import React, { Component, Fragment } from "react";
+import { toast } from "react-toastify";
+import { values } from "sequelize/types/lib/operators";
 import '../App.css';
 
-class FormPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const FormPage = ({ setAuth }) => (
+  <Formik
+    initialValues = {{
       dateOfBirth: "",
       sex: "",
       hairType: "",
       hairLength: "",
       bleach: false,
-      color: false,
-    };
-  }
+      coloring: false,
+    }}
 
-  handleChangeInputdDOB = async event => {
-    const dateOfBirth = event.target.value
-    this.setState({ dateOfBirth })
-  }
-
-  handleChangeInputSex = async event => {
-    const sex = event.target.value
-    this.setState({ sex })
-  }
-
-  handleChangeInputHairType = async event => {
-    const hairType = event.target.value
-    this.setState({ hairType })
-  }
-
-  handleChangeInputHairLength = async event => {
-    const hairLength = event.target.value
-    this.setState({ hairLength })
-  }
-
-  handleChangeInputBleach = async event => {
-    const bleach = event.target.value
-    this.setState({ bleach })
-  }
-
-  handleChangeInputColor = async event => {
-    const color = event.target.value
-    this.setState({ color })
-  }
-
-  handleSubmit = async event => {
-    event.preventDefault();
-    console.log(this.state.hairType);
-    this.setState({
-      dateOfBirth: "",
-      sex: "",
-      hairType: "",
-      hairLength: "",
-      bleach: "",
-      color: "",
-    });
-  }
-
-    render() { 
+    onSubmit = {async (values, { setSubmitting }) => {
+      try {
+        const res = await fetch("/profile/edit", {
+          method: "PUT",
+          headers: {
+            headers: {
+              token: localStorage.token,
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(values)
+          }
+        });
+        toast.success("Updated!");
+      }
+      catch (err){
+        console.error(err.message);
+      }
+    }}
+  >
+    {(props) => {
+      const {
+        values,
+        isSubmitting,
+        handleChange,
+        handleSubmit,
+      } = props;
+    
       return (
-        <div className="content">
-        <div className="container-fluid">
-          <div className="calendar-main-panel">
-            <div className="row justify-content-md-center">
-              <div className="col-md-5 my-auto">
-                <div className="card register-card h-100">
-                  <div className="card-header">Edit details</div>
-                  <div className="card-body">
-                    <form onSubmit = {this.handleSubmit}>
-                      <div class="form-group">
-                        <label for="dateOfBirth">Date of Birth</label>
-                        <input
-                          type="date"
-                          class="form-control"
-                          name="dateOfBirth"
-                          onChange = {this.handleChangeInputdDOB}
-                        ></input>
-                      </div>
-                      <div class="form-group">
-                        <label for="sex">Sex</label>
-                        <select class="form-control" id="sex" onChange = {this.handleChangeInputSex}>
-                          <option>Male</option>
-                          <option>Female</option>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label for="hairType">Hair Type</label>
-                        <select class="form-control" id="hairType" onChange = {this.handleChangeInputHairType}>
-                          <option>Straight</option>
-                          <option>Wavy</option>
-                          <option>Curly</option>
-                          <option>Coily</option>
-                        </select>
-                      </div>
-                      <div className="row form-group">
-                          <div className="col-md-4">
-                            <label for="hairLength">Estimated Hair Length</label>
-                          </div>
-                          <div className="col-lg-1 col-md-2">
+        <Fragment>
+          <div className="content">
+            <div className="container-fluid">
+              
+                <div className="row justify-content-md-center">
+                  <div className="col-md-5 my-auto">
+                    <div className="card register-card h-100">
+                      <div className="card-header">Edit details</div>
+                      <div className="card-body">
+                        <form onSubmit = {handleSubmit}>
+                          <div class="form-group">
+                            <label for="dateOfBirth">Date of Birth</label>
                             <input
+                            type="date"
+                            class="form-control"
+                            name="dateOfBirth"
+                            value = {values.dateOfBirth}
+                            onChange = {handleChange}
+                            ></input>
+                          </div>
+                          <div class="form-group">
+                            <label for="sex">Sex</label>
+                            <select class="form-control" id="sex" onChange = {handleChange}>
+                              <option value = {values.sex}>Male</option>
+                              <option value = {values.sex}>Female</option>
+                            </select>
+                          </div>
+                          <div class="form-group">
+                          <label for="hairType">Hair Type</label>
+                            <select class="form-control" id="hairType" onChange = {handleChange}>
+                              <option value = {values.hairType}>Straight</option>
+                              <option value = {values.hairType}>Wavy</option>
+                              <option value = {values.hairType}>Curly</option>
+                              <option value = {values.hairType}>Coily</option>
+                            </select>
+                          </div>
+                          <div className="row form-group">
+                            <div className="col-md-4">
+                              <label for="hairLength">Estimated Hair Length</label>
+                            </div>
+                            <div className="col-lg-1 col-md-2">
+                              <input
                               type="number"
                               class="form-control-inline hairLength"
                               name="hairLength"
-                              onChange = {this.handleChangeInputHairLength}
+                              value = {values.hairLength}
+                              onChange = {handleChange}
                               style={{width: 45 + 'px'}}
-                            ></input>
+                              ></input>
+                            </div>
+                            <div className="col-lg-7 col-md-6">
+                              <span> inches</span>
+                            </div>
                           </div>
-                          <div className="col-lg-7 col-md-6">
-                            <span> inches</span>
+                          <div className="row form-group">
+                            <div className="col-md-4">
+                              <label>History of bleaching</label>
+                            </div>
+                            <div className="col-md-2">
+                              <div>
+                                <input className="form-check-input" type="radio" name="bleached" id="bleachYes" value={values.bleach} onChange = {handleChange}></input>
+                                <label className="form-check-label" for="bleachYes">Yes</label>
+                              </div>
+                            </div>
+                            <div className="col-md-2">
+                              <div>
+                                <input className="form-check-input" type="radio" name="bleached" id="bleachNo" value={values.bleach} onChange = {handleChange}></input>
+                                <label className="form-check-label" for="bleachNo">No</label>
+                              </div>
+                            </div>
                           </div>
+
+                          <div className="row form-group">
+                            <div className="col-md-4">
+                              <label>History of coloring</label>
+                            </div>
+                            <div className="col-md-2">
+                              <div>
+                                <input className="form-check-input" type="radio" name="colored" id="colorYes" value={values.coloring} onChange = {handleChange}></input>
+                                <label className="form-check-label" for="colorYes">Yes</label>
+                              </div>
+                            </div>
+                            <div className="col-md-2">
+                              <div>
+                                <input className="form-check-input" type="radio" name="colored" id="colorNo" value={values.coloring} onChange = {handleChange}></input>
+                               <label className="form-check-label" for="colorNo">No</label>
+                              </div>
+                            </div>
+                          </div>
+                          <button 
+                            type="submit" 
+                            className="btn btn-primary"
+                            disabled = {isSubmitting}
+                          >
+                            Submit
+                          </button>
+                        </form>
                       </div>
-                      <div className="row form-group">
-                        <div className="col-md-4">
-                          <label>History of bleaching</label>
-                        </div>
-                        <div className="col-md-2">
-                          <div>
-                            <input className="form-check-input" type="radio" name="bleached" id="bleachYes" value="true" onChange = {this.handleChangeInputBleach}></input>
-                            <label className="form-check-label" for="bleachYes">Yes</label>
-                          </div>
-                        </div>
-                        <div className="col-md-2">
-                          <div>
-                            <input className="form-check-input" type="radio" name="bleached" id="bleachNo" value="false" onChange = {this.handleChangeInputBleach}></input>
-                            <label className="form-check-label" for="bleachNo">No</label>
-                          </div>
-                        </div>
-                      </div>
-          
-                      <div className="row form-group">
-                        <div className="col-md-4">
-                          <label>History of coloring</label>
-                        </div>
-                        <div className="col-md-2">
-                          <div>
-                            <input className="form-check-input" type="radio" name="colored" id="colorYes" value="true" onChange = {this.handleChangeInputColor}></input>
-                            <label className="form-check-label" for="colorYes">Yes</label>
-                          </div>
-                        </div>
-                        <div className="col-md-2">
-                          <div>
-                            <input className="form-check-input" type="radio" name="colored" id="colorNo" value="false" onChange = {this.handleChangeInputColor}></input>
-                            <label className="form-check-label" for="colorNo">No</label>
-                          </div>
-                        </div>
-                      </div>
-                      <button type="submit" className="btn btn-primary">
-                        Submit
-                      </button>
-                    </form>
+                    </div>
                   </div>
                 </div>
-              </div>
             </div>
           </div>
-        </div>
-      </div>
-      )
-    }
-}
+        </Fragment>
+      );
+    }}
+  </Formik>
+);
 
-export default FormPage
+export default FormPage;
--- routes/profile.js
+++ routes/profile.js
@@ -21,7 +21,7 @@ router.post("/", authorization, async (req, res) => {
 //Update profile image and username
 router.put("/update", authorization, async (req, res) => {
   try {
-    const { profileImage, newName } = req.body;
+    const { profileImage, newName, dateOfBirth, sex, hairType, hairLength, bleach, coloring } = req.body;
 
     const user = await User.findOne({ where: { id: `${req.user}` } });
 
@@ -34,6 +34,16 @@ router.put("/update", authorization, async (req, res) => {
       user.profileImage = profileImage;
       await user.save();
     }
+    
+    user.dateOfBirth = dateOfBirth;
+    user.sex = sex;
+    user.hairType = hairType;
+    user.hairLength = hairLength;
+    user.bleach = bleach;
+    user.coloring = coloring;
+
+    await user.save();
+
     res.status(200).json("File uploaded successfully!");
   } catch (err) {
     console.error(err.message);
@@ -75,18 +85,25 @@ router.get("/all", async (req, res) => {
 });
 
 // update user info
-router.get("/edit/:id", async (req, res) => {
+router.get("/edit/", authorization, async (req, res) => {
   try {
     const { dateOfBirth, sex, hairType, hairLength, bleach, coloring } = req.body;
 
     const user = await User.findOne({
       where: {
-        id: req.params.id,
+        id: `${req.user}`
       },
     });
-    user.sex = "Male";
+    
+    user.dateOfBirth = dateOfBirth;
+    user.sex = sex;
+    user.hairType = hairType;
+    user.hairLength = hairLength;
+    user.bleach = bleach;
+    user.coloring = coloring;
+
     await user.save();
-    res.json(user);
+    //res.json(user);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
