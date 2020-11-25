24174b37e9d0e0d5becfd4554fc76f944d860e22
MatthewChristian <matthewjlchristian@yahoo.com>
2020-11-24 22:39:45 -0400

    Added demo data

--- client/src/test.js
+++ client/src/test.js
@@ -1,12 +1,45 @@
 export const testData = [
     {
-        "summary": "Hair Thing - Created Event",
-        "start": {
-        "dateTime": "2020-11-24T19:30:00-04:00"
-        },
-        "end": {
-        "dateTime": "2020-11-24T20:30:00-04:00"
-        }
+        "event": "Wash hair with Shampoo X",
+        "start": "17:00:00",
+        "end": "17:10:00",
+        "day": 0, //Sunday
+        "week": "1"
+    },
+    {
+        "event": "Add Moisturizer Y to hair",
+        "start": "16:00:00",
+        "end": "16:10:00",
+        "day": 3, //Wednesday
+        "week": "1"
+    },
+    {
+        "event": "Brush hair thoroughly",
+        "start": "16:00:00",
+        "end": "16:10:00",
+        "day": 5, //Friday
+        "week": "1"
+    },
+    {
+        "event": "Wash hair with Shampoo Y",
+        "start": "17:00:00",
+        "end": "17:10:00",
+        "day": 1, //Monday
+        "week": "2"
+    },
+    {
+        "event": "Add Moisturizer Z to hair",
+        "start": "16:00:00",
+        "end": "16:10:00",
+        "day": 4, //Thursday
+        "week": "2"
+    },
+    {
+        "event": "Brush hair thoroughly",
+        "start": "16:00:00",
+        "end": "16:10:00",
+        "day": 6, //Saturday
+        "week": "2"
     }
 ];
 
