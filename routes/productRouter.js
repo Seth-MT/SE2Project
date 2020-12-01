86d300cb6e36196d368c08151c3c713e77c92617
Seth Timothy <timothy.seth101@gmail.com>
2020-12-01 12:47:40 -0400

    added style search

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -8,27 +8,6 @@ const { Op } = require("sequelize");
 
 
 /* Product Controllers */
-// add product 
-// router.post("/add", async(req, res) =>{
-//     try {
-//         const { name, brand, description, imageUrl} = req.body;
-       
-//         const newProduct = await Product.create({
-//             name,
-//             brand,
-//             description,
-//             imageUrl
-//         })
-        
-//         res.json({ message: "Product Added"});
-
-//     } catch (err) {
-//         console.error(err.message)
-//         res.status(500).send("Server Error");
-//     }
-// })
-
-
 // get all products
 router.get("/all", async(req, res) => {
     try {
@@ -39,7 +18,6 @@ router.get("/all", async(req, res) => {
         res.status(500).send("Server Error");
     }
 })
-
 
 // search for products using some criteria (name, brand, type)
 router.get("/search/:query", async(req, res) => {
--- routes/styleRouter.js
+++ routes/styleRouter.js
@@ -1,9 +1,14 @@
+// initializes router and db
 const router = require("express").Router();
 const db = require("../db");
+
+// links Style model and imports Op functions
 const Style = require("../models/Style");
+const { Op } = require("sequelize");
+
 const authorization = require("../middleware/authorization");
 
-//Return all posts created
+// return all posts created
 router.post("/", authorization, async (req, res) => {
   try {
     const styles = await Style.findAll();
@@ -13,5 +18,66 @@ router.post("/", authorization, async (req, res) => {
     res.status(500).json("Server Error");
   }
 });
+
+/** 
+ * Searches for hair styles using query parameters that are entered by the user
+ * Uses a get request method to receive data from the server
+ * @author Seth Timothy
+ * @param {Path} "/search/:query" - The target path of the request
+ * @param {Request} req - The user request
+ * @param {Response} res - The server's response
+ * @return {Response} - The response including the data that matches the query
+*/
+
+router.get("/search/:query", async(req, res) => {
+  var type;
+  if (req.params.query.toLowerCase() == "straight") {
+    type = 1;
+  }
+  else if (req.params.query.toLowerCase() == "wavy") {
+    type = 2;
+  }
+  else if (req.params.query.toLowerCase() == "curly") {
+    type = 3;
+  }
+  else if (req.params.query.toLowerCase() == "coily") {
+    type = 4;
+  }
+
+  try {
+      const styles = await Style.findAll({
+          where: {
+              [Op.or]: [
+                  {
+                    name: {
+                      [Op.substring]: req.params.query
+                      }
+                  },
+                  {
+                    type: {
+                      [Op.substring]: req.params.query
+                    }
+                  },
+                  {
+                    hairType: {
+                      [Op.eq]: type
+                    }
+                  },
+                  {
+                    hairLength: {
+                      [Op.substring]: req.params.query
+                    }
+                  }
+              ]
+          }
+      })
+      res.json(styles);
+  }
+  catch (error) {
+      console.error(err.message)
+      res.status(500).send("Server Error");
+  }
+});
+
 
 module.exports = router;
